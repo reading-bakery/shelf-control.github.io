d1b57a3c22e42ea8a2607b13cd4780c5955eae67
K.K. <katjakotschurin@gmail.com>
2025-07-21 03:13:28 +0200

    add charts

--- charts.js
+++ charts.js
@@ -1,3 +1,5 @@
+Chart.register(window['chartjs-plugin-annotation']);
+
 document.addEventListener('DOMContentLoaded', () => {
   const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTXx02YVtknMhVpTr2xZL6jVSdCZs4WN4xN98xmeG19i47mqGn3Qlt8vmqsJ_KG76_TNsO0yX0FBEck/pub?gid=501012815&single=true&output=csv';
 
@@ -29,8 +31,60 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function renderChart(canvasId, label, labels, data, color) {
   const ctx = document.getElementById(canvasId).getContext('2d');
+
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: false,
+        min: canvasId === 'subChart' ? 90 : 0,
+        max: canvasId === 'subChart' ? 150 : undefined,
+        ticks: {
+          stepSize: 10,
+          color: 'white'
+        }
+      },
+      x: {
+        ticks: {
+          color: 'white'
+        }
+      }
+    },
+    plugins: {
+        legend: {
+        position: 'bottom',
+        labels: {
+            color: 'white'
+        }
+        },
+     annotation: canvasId === 'subChart' ? {
+  annotations: {
+    line1: {
+      type: 'line',
+      yMin: 100,
+      yMax: 100,
+      borderColor: 'red',
+      borderWidth: 2,
+      label: {
+        content: 'SuB-Ziel = 100',
+        enabled: true,
+        position: 'start',      
+        color: 'red',
+        font: {
+          size: 12,           
+          weight: 'normal'     
+        },
+        xAdjust: -10,          
+        yAdjust: 0
+      }
+    }
+  }
+} : {}
+    }
+  };
+
   new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
       labels: labels,
       datasets: [{
@@ -38,31 +92,11 @@ function renderChart(canvasId, label, labels, data, color) {
         data: data,
         backgroundColor: color,
         borderColor: color.replace('0.7', '1'),
-        borderWidth: 1
+        borderWidth: 2,
+        fill: false,
+        tension: 0.4 
       }]
     },
-    options: {
-      responsive: true,
-      scales: {
-        y: {
-          beginAtZero: true,
-          ticks: {
-            color: 'white'
-          }
-        },
-        x: {
-          ticks: {
-            color: 'white'
-          }
-        }
-      },
-      plugins: {
-        legend: {
-          labels: {
-            color: 'white'
-          }
-        }
-      }
-    }
+    options: options
   });
 }
