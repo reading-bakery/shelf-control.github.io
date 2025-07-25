b77163552580d5b7d1b8dfde55edad09c2bed4e2
reading-bakery <katjakotschurin@googlemail.com>
2025-07-25 02:41:49 +0200

    charts-verlag.js aktualisieren

--- charts-verlag.js
+++ charts-verlag.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
           label: 'Anzahl Bücher',
           data: data,
           backgroundColor: generateColors(data.length),
-          borderRadius: [15, 15, 15, 15],
+          borderRadius: [20, 20, 20, 20],
           borderWidth: 7,
           barThickness: barThickness
         }]
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
             align: 'right',
             offset: -7,
             clamp: true,
-            font: { weight: 'normal', size: 13 }
+            font: { family: "'Dosis', sans-serif", weight: 'normal', size: 13 }
           }
         }
       },
