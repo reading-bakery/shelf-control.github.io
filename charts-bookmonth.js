9e4d474ae044980662198aafb0ea033e70e8a4ef
reading-bakery <katjakotschurin@googlemail.com>
2025-07-26 12:37:43 +0200

    charts-bookmonth.js aktualisieren

--- charts-bookmonth.js
+++ charts-bookmonth.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
           y: {
             display: false,
             min: 0,
-            max: 13,
+            max: 15,
             grid: { display: true },
             ticks: {
               display: true,
